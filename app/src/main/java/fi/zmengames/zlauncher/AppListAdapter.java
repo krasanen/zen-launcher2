8ca309c7b606e90f05b66d31122080e9bbfc98db
Kari Rasanen <kari.rasanen@zmengames.com>
2019-01-25 17:38:50 +0200

    ViewHolder optimization to listview

--- app/src/main/java/fi/zmengames/zlauncher/AppListAdapter.java
+++ app/src/main/java/fi/zmengames/zlauncher/AppListAdapter.java
@@ -1,9 +1,9 @@
 package fi.zmengames.zlauncher;
 
 import android.annotation.TargetApi;
-import android.app.Application;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +15,9 @@ import android.widget.TextView;
 import java.util.Collection;
 import java.util.List;
 
-import fr.neamar.kiss.IconsHandler;
 import fr.neamar.kiss.KissApplication;
 import fr.neamar.kiss.MainActivity;
 import fr.neamar.kiss.R;
-import fr.neamar.kiss.pojo.AppPojo;
 import fr.neamar.kiss.pojo.Pojo;
 import fr.neamar.kiss.result.Result;
 
@@ -29,6 +27,7 @@ import fr.neamar.kiss.result.Result;
 public class AppListAdapter extends ArrayAdapter<Pojo> {
     private final LayoutInflater mInflater;
     MainActivity mainActivity;
+
     public AppListAdapter(Context context) {
         super(context, android.R.layout.simple_list_item_2);
         mainActivity = KissApplication.getApplication(getContext()).getMainActivity();
@@ -48,32 +47,80 @@ public class AppListAdapter extends ArrayAdapter<Pojo> {
         //If the platform supports it, use addAll, otherwise add in loop
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             super.addAll(items);
-        }else{
-            for(Pojo item: items){
+        } else {
+            for (Pojo item : items) {
                 super.add(item);
             }
         }
     }
 
+
     /**
      * Populate new items in the list.
      */
-    @Override public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final View view;
+        final ViewHolder holder;
 
         if (convertView == null) {
             view = mInflater.inflate(R.layout.list_item_icon_text, parent, false);
+            holder = new ViewHolder();
+            holder.icon = (ImageView) view.findViewById(R.id.icon);
+            holder.icon.setVisibility(View.GONE);
+            holder.text = (TextView) view.findViewById(R.id.text);
+            holder.text.setVisibility(View.GONE);
+            holder.context = getContext();
+            // initialize views
+            view.setTag(holder);  // set tag on view
         } else {
             view = convertView;
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        Pojo item = getItem(position);
-
-        Result result = Result.fromPojo(mainActivity, item);
-        Drawable drawable = result.getDrawable(mainActivity);
-        ((ImageView)view.findViewById(R.id.icon)).setImageDrawable(drawable);
-        ((TextView)view.findViewById(R.id.text)).setText(result.toString());
+        if (holder != null) {
+            new ImageLoadTask(position).execute(holder);
+        }
 
         return view;
     }
+
+    static class ViewHolder {
+        TextView text;
+        ImageView icon;
+        Context context;
+    }
+
+    private class ImageLoadTask extends AsyncTask<ViewHolder, Void, Drawable> {
+        private final int mPosition;
+        private ViewHolder v;
+        private String text;
+        private ImageLoadTask(int position) {
+            this.mPosition = position;
+
+        }
+
+        @Override
+        protected Drawable doInBackground(ViewHolder... params) {
+            v = params[0];
+            Pojo item = getItem(mPosition);
+            final Result result = Result.fromPojo(null, item);
+            this.text = item.getName();
+            return result.getDrawable(v.context);
+        }
+
+        @Override
+        protected void onPostExecute(Drawable result) {
+            super.onPostExecute(result);
+            // If this item hasn't been recycled already, hide the
+            // progress and set and show the image
+            v.text.setText(text);
+            v.text.setVisibility(View.VISIBLE);
+            v.icon.setVisibility(View.VISIBLE);
+            v.icon.setImageDrawable(result);
+
+        }
+    }
+
 }
