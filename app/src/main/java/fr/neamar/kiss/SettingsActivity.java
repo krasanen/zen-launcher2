3675d8a27199f0a8b6513a4da51e652c29b078ee
Kari Rasanen <kari.rasanen@zmengames.com>
2019-09-22 18:27:25 +0300

    * Point help to privacy policy until we have own help

--- app/src/main/java/fr/neamar/kiss/SettingsActivity.java
+++ app/src/main/java/fr/neamar/kiss/SettingsActivity.java
@@ -112,7 +112,7 @@ public class SettingsActivity extends PreferenceActivity implements
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         if (item.getItemId() == R.id.help) {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("http://help.kisslauncher.com"));
+            intent.setData(Uri.parse("https://zmengames.com/zenlauncherprivacypolicy.html"));
             startActivity(intent);
             return true;
         }
