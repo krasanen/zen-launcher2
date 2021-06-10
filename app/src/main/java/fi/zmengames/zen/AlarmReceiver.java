a1e0ff1c58e929c3bf80d700f5f3e112ab47f052
Kari Rasanen <kari.rasanen@zmengames.com>
2021-06-10 23:30:20 +0300

    More Spanish + Finnish translations
    Thanks again Freddy :)

--- app/src/main/java/fi/zmengames/zen/AlarmReceiver.java
+++ app/src/main/java/fi/zmengames/zen/AlarmReceiver.java
@@ -5,16 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import fr.neamar.kiss.R;
+
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent k2) {
         // TODO Auto-generated method stub
-        Toast.makeText(context, "Alarm received!", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, context.getResources().getString(R.string.alarm_received), Toast.LENGTH_LONG).show();
         Intent intent = new Intent(context, AlarmActivity.class);
         intent.putExtras(k2);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
-
     }
 }
