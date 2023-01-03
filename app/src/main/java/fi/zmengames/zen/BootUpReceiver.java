9ba87af1d46547b6b6c7b068ead94d0194231913
Kari Rasanen <kari.rasanen@zmengames.com>
2023-01-03 17:49:52 +0200

    Replace media mount listener with bootup complete listener to get SD card installed apps added to list.
    * Media mount listener was fired multiple times during bootup phase and caused unnecessary extra load to phone.
    
    * Note, it takes some time after phone seems to be fully booted up before the bootup intent fires.
    
    * reference to KISS issue dealing with the same thing, https://github.com/Neamar/KISS/issues/1847

--- app/src/main/java/fi/zmengames/zen/BootUpReceiver.java
+++ app/src/main/java/fi/zmengames/zen/BootUpReceiver.java
@@ -1,22 +1,19 @@
 package fi.zmengames.zen;
-
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+
 import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
 
-import fr.neamar.kiss.BuildConfig;
+public class BootUpReceiver extends BroadcastReceiver {
+    private static final String TAG = BootUpReceiver.class.getSimpleName();
 
-public class MediaMountListener extends BroadcastReceiver {
-    private static final String TAG = MediaMountListener.class.getSimpleName();
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (BuildConfig.DEBUG) Log.d(TAG, "Media mounted, reload apps, action:"+intent.getAction());
-
         ZEvent event = new ZEvent(ZEvent.State.RELOAD_APPS);
-        EventBus.getDefault().postSticky(event);
-
+        EventBus.getDefault().post(event);
     }
 }
